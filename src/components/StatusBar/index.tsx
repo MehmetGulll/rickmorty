1d7937bafc1bbd5902520f5114dd927f043e47bf
MehmetGulll <97182277+MehmetGulll@users.noreply.github.com>
2022-12-27 14:21:57 +0300

    StatusBar

---
+++ src/components/StatusBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function StatusBar() {
+  return (
+    <div className='container3'>
+            <div className='status'>
+                Status
+            </div>
+            <div className='male'>Male</div>
+    </div>
+  )
+}
+
+export default StatusBar
